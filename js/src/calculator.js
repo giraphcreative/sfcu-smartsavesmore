21789a5626da5d649c06f490fb4a4255b36fcfab
James Pederson <james@jpederson.com>
2015-07-29 16:25:25 -0500

    Making changes to fit Smart FCU's brand.

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -3,13 +3,12 @@
 $(function(){
 	
 	var recalculate_total = function(){
-			var total = parseFloat( typeof( $(".result.home").html() )!=="undefined" ? $(".result.home").html().replace("$","").replace(",","") : 0 )+
-				parseFloat( typeof( $(".result.loan-auto").html() )!=="undefined" ? $(".result.loan-auto").html().replace("$","").replace(",","") : 0 )+
-				parseFloat( typeof( $(".result.loan-personal").html() )!=="undefined" ? $(".result.loan-personal").html().replace("$","").replace(",","") : 0 );
-			if ( total>0 ) {
-				$(".result.total").html(""+total.toFixed(2));
-			}
-		};
+		var total = parseFloat( typeof( $(".result.home").html() )!=="undefined" ? $(".result.home").html().replace("$","").replace(",","") : 0 )+
+			parseFloat( typeof( $(".result.loan-auto").html() )!=="undefined" ? $(".result.loan-auto").html().replace("$","").replace(",","") : 0 );
+		if ( total>0 ) {
+			$(".result.total").html(""+total.toFixed(2));
+		}
+	};
 
 	$(".calculator.home").accrue({
 		mode: "compare",
@@ -26,18 +25,6 @@ $(function(){
 	$(".calculator.loan-auto").accrue({
 		mode: "compare",
 		response_output_div: ".result.loan-auto",
-		response_compare:"%savings%",
-		error_text:"0",
-		callback: function( elem, data ){
-			if ( data!==0 ) {
-				recalculate_total();
-			}
-		}
-	});
-
-	$(".calculator.loan-personal").accrue({
-		mode: "compare",
-		response_output_div: ".result.loan-personal",
 		response_compare:"%savings%",
 		error_text:"0",
 		callback: function( elem, data ){
