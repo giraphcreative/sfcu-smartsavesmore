275697f4eba8708bb731cb609f357387d0069e1d
James Pederson <james@jpederson.com>
2017-04-25 18:37:15 -0500

    Update layout styles and images, add send form.

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -1,54 +1,22 @@
 
 
 $(function(){
+
+	$(".calculator.auto").accrue({
+		mode: "compare",
+		response_output_div: ".result.auto",
+		response_compare: "<h3>Congratulations!<br> You can save up to <strong>$%savings%</strong> in interest!</h3>",
+		error_text: "<p>Enter loan information to calculate savings.</p>"
+	});
 	
-	var recalculate_total = function(){
-		var total = parseFloat( typeof( $(".result.loan-auto").html() )!=="undefined" ? $(".result.loan-auto").html().replace("$","").replace(",","") : 0 );
-		if ( total>0 ) {
-			$(".result.total").html(""+total.toFixed(2));
-		}
-	};
-
-	/*
-	parseFloat( typeof( $(".result.home").html() )!=="undefined" ? $(".result.home").html().replace("$","").replace(",","") : 0 )+
-
 	$(".calculator.home").accrue({
 		mode: "compare",
 		response_output_div: ".result.home",
-		response_compare:"%savings%",
-		error_text:"0",
-		callback: function( elem, data ){
-			if ( data!==0 ) {
-				recalculate_total();
-			}
-		}
-	});
-	*/
-
-	$(".calculator.loan-auto").accrue({
-		mode: "compare",
-		response_output_div: ".result.total",
-		response_compare:"%savings%",
-		error_text:"0"
+		response_compare: "<h3>Congratulations!<br> You can save up to <strong>$%savings%</strong> in interest!</h3>",
+		error_text: "<p>Enter loan information to calculate savings.</p>"
 	});
 
-	$(".numbers-only").keyup(function(){
-		var fixed=$(this).val().replace(/[^0-9.]/g,"");
-		$(this).val( fixed );
-	});
-
-	$(".calculate").click(function(){
-		$(".tool").slideUp( 'slow' );
-		$(".results").slideDown( 'slow' );
-		$('html,body').animate({
-			scrollTop: $( "header img" ).height()
-        }, 800);
-	});
-
-	$(".go-back").click(function(){
-		$(".tool").slideDown( 'slow' );
-		$(".results").slideUp( 'slow' );
-	});
+	$('a').creep();
 
 });
 
