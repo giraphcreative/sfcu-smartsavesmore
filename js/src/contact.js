8355d066bbf542ff8ffa0dbf08914b186086ea17
James Pederson <james@jpederson.com>
2017-04-25 18:43:51 -0500

    Update contact.js to include the message field.

--- js/src/contact.js
+++ js/src/contact.js
@@ -18,7 +18,7 @@ var contact_submit = function( form ) {
 			name: $( form ).find( "input[name=name]" ).val(),
 			email: $( form ).find( "input[name=email]" ).val(),
 			phone: $( form ).find( "input[name=phone]" ).val(),
-			message: $( form ).find( "input[name=message]" ).val(),
+			message: $( form ).find( "textarea[name=message]" ).val(),
 		},
 		url_params = $.param( field ),
 		errors = [],
